30ae5ec897c027c7acdf950f7e438ac6937e81c6
shebz2023 <shebelleke@gmail.com>
2025-02-22 22:06:54 +0200

    [add]mybookings :dashboard

--- src/pages/user/MyBookings.tsx
+++ src/pages/user/MyBookings.tsx
@@ -1,28 +1,43 @@
+import { MY_BOOKINGS } from "../../services/api";
+import { useQuery } from "@apollo/client";
+import LoadingSkeleton from "../../components/LoadingSkeleton";
 
 export default function MyBookings() {
-  const bookings = [
-    { id: 1, name: "Beachfront Villa", status: "Pending" },
-    { id: 2, name: "City Center Apartment", status: "canceled" },
-  ];
+  interface MyBooking {
+    id: string;
+    property: Property;
+    location: string;
+    status: string;
+  }
+  interface Property{
+    title:string;
+  }
+
+  const { loading, error, data } = useQuery(MY_BOOKINGS);
+
+  if (loading) return <LoadingSkeleton />;
+  if (error) return `Error! ${error.message}`;
+  console.log(data, "my bookings");
+
   return (
     <>
       <div className="w-full text-white">
         <div className="bg-black p-6 rounded-lg shadow-lg">
           <h2 className="text-xl font-semibold mb-6">My bookings</h2>
-          {bookings.length === 0 ? (
+          {data?.myBookings.length === 0  ? (
             <p className="text-neutral-400">
               You haven't added any bookings yet. Start by adding one!
             </p>
           ) : (
             <ul className="space-y-4">
-              {bookings.map((booking) => (
+              {data.myBookings.map((myBooking: MyBooking) => (
                 <li
-                  key={booking.id}
+                  key={myBooking.id}
                   className="flex justify-between items-center bg-neutral-900 p-4 rounded-lg"
                 >
                   <div>
-                    <h3 className="text-lg font-semibold">{booking.name}</h3>
-                    <p className="text-neutral-300">{booking.status}</p>
+                    <h3 className="text-md font-semibold tracking-widest font-poppins">{myBooking.property.title}</h3>
+                    <p className="text-neutral-300">{myBooking.status}</p>
                   </div>
                   <div className="flex gap-4">
                     <button className="cursor-pointer bg-black border border-neutral-950 text-white py-1 px-3 rounded hover:bg-neutral-950 hover:border-neutral-600">
--- src/services/api.ts
+++ src/services/api.ts
@@ -28,10 +28,22 @@ export const MY_PROPERTIES = gql`
     }
   }
 `;
-export const DELETE_PROPERTY = `
+export const DELETE_PROPERTY = gql`
   mutation DeleteProperty($deletePropertyId: ID!) {
     deleteProperty(id: $deletePropertyId) {
       id
+    }
+  }
+`;
+
+export const MY_BOOKINGS = gql`
+  query MyBookings {
+    myBookings {
+      id
+      status
+      property {
+        title
+      }
     }
   }
 `;
